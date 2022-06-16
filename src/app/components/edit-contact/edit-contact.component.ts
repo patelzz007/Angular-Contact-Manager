d127f90d5f2b84fb462c326666e3f9d4c8e98355
Bishen Patel <patelzz007@gmail.com>
2022-06-16 17:05:45 +0800

    Initial Commit.

---
+++ src/app/components/edit-contact/edit-contact.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { IContact } from 'src/app/models/IContacts';
+import { IGroup } from 'src/app/models/IGroup';
+import { ContactService } from 'src/app/services/contact.service';
+
+@Component({
+  selector: 'app-edit-contact',
+  templateUrl: './edit-contact.component.html',
+  styleUrls: ['./edit-contact.component.css']
+})
+export class EditContactComponent implements OnInit {
+  public loading: boolean = false;
+  public contactId: string | null = null;
+  public contact: IContact = {} as IContact;
+  public errorMessage: string | null = null;
+  public groups: IGroup[] = {} as IGroup[];
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private contactService: ContactService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((param:ParamMap) => {
+      this.contactId = param.get('contactId');
+    });
+    if(this.contactId) {
+      this.contactService.getContact(this.contactId).subscribe((data:IContact) => {
+        this.contact = data;
+        this.loading = false;
+        this.contactService.getAllGroups().subscribe((data:IGroup[]) => {
+          this.groups = data;
+        })
+      }, (error) => {
+        this.errorMessage = error;
+        this.loading = false;
+      });
+    }
+  }
+
+  public submitUpdate() {
+    if(this.contactId) { // if contactId is available
+      this.contactService.updateContact(this.contact, this.contactId).subscribe((data: IContact) => { // submit data to service
+        this.router.navigate(['/']).then(); // navigate to homepage
+      }, (error) => {
+        this.errorMessage = error;
+        this.router.navigate(['/contacts/edit/${this.contactId}']).then();
+      });
+    }
+  }
+
+}
---
+++ src/app/models/IContacts.ts
@@ -0,0 +1,10 @@
+export interface IContact {
+    id?: string; // optional (might be undefined)
+    name: string;
+    email:  string;
+    photo: string;
+    mobile: string;
+    company: string;
+    title: string;
+    groupId: string;
+}
